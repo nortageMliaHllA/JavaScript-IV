fc65294ed166a0fd43629b09cff6730509805105
AllHailMegatron <ahmadrthomp@gmail.com>
2019-05-09 22:45:39 -0400

    Checked my code against the solution code and made corrections to my lambda-classes.js file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,7 +8,7 @@ class Person {
         this.gender = attributes.gender;
     }
     speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}`
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
     }
 }
 
@@ -18,33 +18,34 @@ class Instructor extends Person {
         this.specialty = instructorattr.specialty;
         this.favLanguage = instructorattr.favLanguage;
         this.catchPhrase = instructorattr.catchPhrase;
-        this.name = instructorattr.name;
     }
-    demo() {
-        return `Today we are learning about ${this.favLanguage}`
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
     }
-    grade() {
-        return `${this.name} receives a perfect score on ${this.favLanguage}`
+    grade(student, subject) {
+        console.log(`${this.name} receives a perfect score on ${subject}`);
     }
 }
 
 class Student extends Person {
     constructor(studentattr) {
         super(studentattr);
-        this.name = studentattr.name;
         this.previousBackground = studentattr.previousBackground;
         this.className = studentattr.className;
         this.favSubjects = studentattr.favSubjects;
         
     }
     listsSubjects() {
-        return `${this.favSubjects}`
+        for (let i = 0; i< this.favSubjects.length; i++) {
+            console.log(`${this.name} ${this.favSubjects[i]}`);
+        }
+        
     }
-    PRAssignment() {
-        return `${this.name} has submitted a PR for ${this.favSubjects}`
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`);
     }
-    sprintChallenge() {
-        return `${this.name} has begun sprint challenge on ${this.favSubjects}`
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
     }
 }
 
@@ -54,11 +55,11 @@ class ProjectManagers extends Instructor {
         this.gradClassName = pmattr.gradClassName;
         this.favInstructor = pmattr.favInstructor;
     }
-    standUp() {
-        return `${this.name} announces to ${this.gradClassName}, standy times!`;
+    standUp(channel) {
+        console.log(`${name} announces to ${channel}, @channel standy times!`);
     }
-    debugsCode() {
-        return `${this.name} debugs ${this.name}'s code on ${this.favSubjects}`
+    debugsCode(student, subject) {
+        console.log(`${name} debugs ${this.name}'s code on ${subject}`);
     }
 }
 
@@ -71,27 +72,38 @@ const fred = new Instructor({
     specialty: 'Front-end',
     catchPhrase: `Don't forget the homies`
   });
-
-//   const wilma = new Instructor({
-//       name: 'wilma',
-//       favLanguage: 'Python',
-//   });
   
 const wilma = new Student({
     name: 'Wilma',
+    location: 'LA',
+    age: 40,
+    gender: 'female',
     previousBackground: 'Customer Service',
     className: 'WEBpt6_sprint03',
-    favSubjects: ['HTML', ' CSS', ' JavaScript']
+    favSubjects: ['HTML', 'CSS', 'JavaScript']
 });
-  
 
+const john = new ProjectManagers({
+    name: 'John',
+    location: 'Yonkers',
+    age: 27,
+    gender: 'male',
+    gradClassName: 'CS5',
+    favInstructor: 'Willis',
+    favLanguage: 'Elm',
+    specialty: 'Front-end',
+    catchPhrase: `Down Elm street we go!`
+})
   
-  
-  
-  
-  
-  console.log(fred.speak());
-  console.log(fred.demo());
-  console.log(fred.grade());
-  console.log(wilma.listsSubjects());
-  console.log(wilma.PRAssignment());
+  fred.speak();
+  fred.demo('strings');
+  fred.grade('wilma', 'CSS');
+  wilma.speak();
+  wilma.listsSubjects();
+  wilma.PRAssignment('HMTL');
+  wilma.sprintChallenge('JavaScript Foundations');
+  john.speak();
+  john.demo('CSS');
+  john.grade('wilma', 'functions vs methods');
+  john.debugsCode('wilma', 'classes');
+  john.standUp('Code-Newbie');
